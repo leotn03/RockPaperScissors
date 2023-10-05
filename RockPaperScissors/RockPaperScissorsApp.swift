49510e7ef29ae825b49f544b7f688565ba834f48
leotn03 <leo.tn03@icloud.com>
2023-10-04 20:24:48 -0500

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Leo Torres Neyra on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
